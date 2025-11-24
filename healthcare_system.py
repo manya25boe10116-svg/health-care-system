b15b01a64ce5152058222908d0a16246ee0caf19
manya25boe10116-svg <manya.25boe10116@vitbhopal.ac.in>
2025-11-25 01:04:26 +0530

    Add files via upload

---
+++ healthcare_system.py
@@ -0,0 +1,34 @@
+while True:
+      height=float(input("Enter height in meters"))
+
+      weight=float(input("enter weight in kilogram"))
+
+      BMI=(weight/(height*height))
+      print(f"your body mass index bmi is {BMI}")
+      if (BMI<=18.5):
+          print("you are underweight eat nutritious food")
+      elif(BMI  <=24.9):
+          print("you are  healthy")
+      
+      elif(BMI<=29.9):
+          print ("you are overweight do exercise regularly and eat an healthy food")
+      else:
+          print("you are obse meet the doctor")
+      userwantstocontinue =input("do you want to contine? yes or no ")
+      if(userwantstocontinue =='no'):
+          print("over")
+          break
+
+
+
+
+
+
+
+
+
+
+
+        
+
+      
